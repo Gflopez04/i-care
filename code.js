92ee0394d776ae5b67a67b7de19fc89a552199dc
gabriel <gflopez04@gmail.com>
2021-11-18 14:04:33 -0500

    Agregacion funcionalidad mostrar password

---
+++ code.js
@@ -0,0 +1,13 @@
+var state=flase;
+function toggle(){
+    if(state){
+        document.getElementById("password").setAttribute("type","password");
+        document.getElementById("eye").style.color='#d7ddd8';
+        state=false;
+    }
+    else{
+        document.getElementById("password").setAttribute("type","text");
+        document.getElementById("eye").style.color='#88c999';
+        state=true;
+    }
+}
